1ba402c9a1f9752dd866e757ce52f381c8ba18bc
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2020-08-30 14:09:42 +0200

    Local formatting test

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -euo pipefail
+shopt -s globstar
 
+echo "Formatting"
+
+diff -u <(cat **/*.{cpp,hpp,ino}) <(clang-format -style=Google **/*.{cpp,hpp,ino})
+
+echo "Linting"
 . .venv/bin/activate
 python -m cpplint --filter=-legal/copyright wecker.cpp src/**/*pp
 
-echo "Compiling for PC..."
+echo "Compiling for PC
 make clean
 make pc
 echo "Success"
